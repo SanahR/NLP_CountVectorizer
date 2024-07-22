fcb52c678cb1817547bae2aa72a8c72850d93912
SanahR <136401567+SanahR@users.noreply.github.com>
2024-07-21 19:57:52 -0500

    Adding Comments
    Commenting on major lines to clarify what's happening in the program.

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import random
 from sklearn.feature_extraction.text import CountVectorizer
 
 #Step 1: Creating the Data
+#Asking the user for a review of a movie
 movie_review = input("Hello, please provide a short review of the most recent movie you watched. ")
+#Creating the list that the review will be added to. 
 test_corpus = ["hate","I like this, but the production quality is terrible.","Adore","good","good"]
+#Adding the review to the test database
 test_corpus.append(movie_review)
+#Creating a short list of descriptive words to train the computer on. 
 training_corpus = ['hate','like','terrible','adore','love','good','bad','amazing','horrible','great','atrocious','perfect','sucky','flawless']
 
 #Step 2: Creating the Algorithm
@@ -17,8 +21,11 @@ vectorizer = CountVectorizer()
 vectorizer.fit(training_corpus)
 
 #Step 4: Doing the actual Transformation
+#Transforming the words in the training list to numbers
 X = vectorizer.transform(training_corpus)
+#Sending the number versions of the list to an array
 X_train = X.toarray()
+#Repeating the same process of vectorizing the test list(with the user's review) and sending it to an array
 X_test = vectorizer.transform(test_corpus)
 X_test = X_test.toarray()
 
