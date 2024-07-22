ef1eecc4b96f3b8aa7cd5878ecc140bfe04d5a87
SanahR <136401567+SanahR@users.noreply.github.com>
2024-07-21 20:03:20 -0500

    Adding Headings
    Using hashmarks to make the distinctions between steps more clear.

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import random
 from sklearn.feature_extraction.text import CountVectorizer
 
-#Step 1: Creating the Data
+#######################################
+#######Step 1: Creating the Data#######
+#######################################
+
 #Asking the user for a review of a movie
 movie_review = input("Hello, please provide a short review of the most recent movie you watched. ")
 #Creating the list that the review will be added to. 
@@ -14,13 +17,19 @@ test_corpus.append(movie_review)
 #Creating a short list of descriptive words to train the computer on. 
 training_corpus = ['hate','like','terrible','adore','love','good','bad','amazing','horrible','great','atrocious','perfect','sucky','flawless']
 
-#Step 2: Creating the Algorithm
+##########################################
+######Step 2: Creating the Algorithm######
+##########################################
 vectorizer = CountVectorizer()
 
-#Step 3: Training the Vectorizer
+###########################################
+######Step 3: Training the Vectorizer######
+###########################################
 vectorizer.fit(training_corpus)
 
-#Step 4: Doing the actual Transformation
+###################################################
+######Step 4: Doing the actual Transformation######
+###################################################
 #Transforming the words in the training list to numbers
 X = vectorizer.transform(training_corpus)
 #Sending the number versions of the list to an array
@@ -29,7 +38,9 @@ X_train = X.toarray()
 X_test = vectorizer.transform(test_corpus)
 X_test = X_test.toarray()
 
-#Machine Learning Portion
+####################################
+######Machine Learning Portion######
+####################################
 #Step 1: Creating the Data(data transformation is already done)
 y_train = np.array(["bad","good","bad","good","good","good",'bad','good','bad','good','bad','good','bad','good'])
 #Creating the Algorithm
